456e78ca6621f37226efb888667427188cbd1930
srikanth458hk <125630483+srikanth458hk@users.noreply.github.com>
2023-07-11 12:33:52 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker pull public.ecr.aws/y6s3n1h9/nodejs:latest
-sudo docker run -itd --network=host public.ecr.aws/y6s3n1h9/nodejs:latest
+ docker pull public.ecr.aws/y6s3n1h9/nodejs:latest
+ docker run -itd --network=host public.ecr.aws/y6s3n1h9/nodejs:latest
